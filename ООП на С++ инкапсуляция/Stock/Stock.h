7e2b2f4514b1ee2d482d9984c64f6dd09465c53f
Vladbazy <bazyak.vadim@gmail.com>
2025-06-20 12:42:42 +0700

    Add files via upload

---
+++ ООП на С++ инкапсуляция/Stock/Stock.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <vector>
+
+class Stock {
+    struct Box {
+        int id;
+        int weight;
+        int volume;
+        Box(int id, int w, int v) : id(id), weight(w), volume(v) {}
+    };
+
+    std::vector<Box*> boxes;
+    int next_id;
+
+public:
+    Stock();
+    ~Stock();
+
+    void Add(int w, int v);
+    int GetByW(int min_w);
+    int GetByV(int min_v);
+};
